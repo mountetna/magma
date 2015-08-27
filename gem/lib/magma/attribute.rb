1658af2300679980650cd6cef9bafb7516d72ed7
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2015-08-27 19:44:09 +0000

    fix array validation problem

--- gem/lib/magma/attribute.rb
+++ gem/lib/magma/attribute.rb
@@ -45,7 +45,7 @@ class Magma
           yield error
         end
       when Array
-        if !@match.include value
+        if !@match.include? value
           error = "'#{value}' should be one of #{@match.join(", ")}."
           yield error
         end
