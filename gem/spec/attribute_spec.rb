b8b81c24c36d0b153a61965830032da07be7a3ca
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2016-04-01 19:25:55 +0000

    add empty specs

---
+++ gem/spec/attribute_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../lib/magma'
+require 'yaml'
+
+db_config = YAML.load File.read("../../config.yml")
+Magma.instance.connect db_config[:database]
+
+describe Magma::Attribute do
+  describe ".json_template" do
+    it "returns a json template describing the attribute" do
+    end
+  end
+
+  describe ".json_for" do
+    it "returns a json object describing a value from a record" do
+    end
+  end
+
+  # this is probably unnecessary
+  describe ".entry_for" do
+    it "returns a valid entry value for this record" do
+    end
+  end
+
+  describe ".validate" do
+    it "checks validations for a given value and record" do
+    end
+  end
+end
--- gem/spec/magma_spec.rb
+++ gem/spec/magma_spec.rb
@@ -1,14 +1,26 @@
-$: << "../lib"
-
-require 'magma'
+require_relative '../lib/magma'
 require 'yaml'
 
-db_config = YAML.load File.read("../../database.yml")
-Magma.instance.connect db_config
+db_config = YAML.load File.read("../../config.yml")
+Magma.instance.connect db_config[:database]
 
-describe Magma::Model do
-  describe ".identifier" do
-    class Patient < Magma::Model
+describe Magma do
+  describe ".connect" do
+    it "creates a Sequel database connection" do
+    end
+  end
+
+  describe ".configure" do
+    it "loads models and validates tables" do
+    end
+  end
+
+  describe ".get_model" do
+    it "returns the class for a given model name" do
+    end
+  end
+  describe ".magma_models" do
+    it "returns a list of all magma models" do
     end
   end
 end
