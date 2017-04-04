e15844ce82629af2d098afd1b908cdd2fe75504a
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2017-04-04 16:48:41 +0000

    update gene-exp

--- lib/models/gene_exp.rb
+++ lib/models/gene_exp.rb
@@ -3,7 +3,8 @@
 class GeneExp < Magma::Model
   parent :rna_seq
   #parent :gene # points to a gene record in a table of genes somewhere, maybe later.
-  attribute :gene_name, type: String, desc: "HGNC gene symbol"
+  attribute :ensembl_name, type: String, desc: "Ensembl gene ID"
+  attribute :hugo_name, type: String, desc: "HGNC gene symbol" # this could be blank
   attribute :read_counts, type: Float, desc: "Raw counts of total reads aligned to this gene."
   attribute :expression, type: Float, desc: "Normalized estimate of gene expression."
 end
