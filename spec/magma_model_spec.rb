eed600ac9d539f8e218367d02478cb7f75926035
Jason Cater <jason.cater@ucsf.edu>
2017-07-06 16:57:56 -0700

    General syntax cleanup and commenting.

--- spec/magma_model_spec.rb
+++ spec/magma_model_spec.rb
@@ -2,101 +2,101 @@ require_relative '../lib/magma'
 require 'yaml'
 
 describe Magma::Model do
-  describe ".validate" do
-    it "ensures the table exists" do
+  describe '.validate' do
+    it 'ensures the table exists' do
     end
   end
 
-  describe ".attributes" do
-    it "returns a hash of attributes for this model" do
+  describe '.attributes' do
+    it 'returns a hash of attributes for this model' do
     end
   end
 
-  describe ".identity" do
-    it "gives the key name for the model's identifier" do
+  describe '.identity' do
+    it 'gives the key name for the model\'s identifier' do
     end
-    it "returns id if model has no identifier" do
-    end
-  end
-
-  describe ".display_attributes" do
-    it "returns the class for a given model name" do
+    it 'returns id if model has no identifier' do
     end
   end
 
-  describe ".order" do
-    it "orders the dataset by column" do
+  describe '.display_attributes' do
+    it 'returns the class for a given model name' do
     end
   end
 
-  describe ".has_attribute?" do
-    it "determines whether an attribute exists" do
+  describe '.order' do
+    it 'orders the dataset by column' do
     end
   end
 
-  context "Attribute descriptions" do
-    describe ".attribute" do
-      it "defines a basic attribute" do
+  describe '.has_attribute?' do
+    it 'determines whether an attribute exists' do
+    end
+  end
+
+  context 'Attribute descriptions' do
+    describe '.attribute' do
+      it 'defines a basic attribute' do
       end
     end
-    describe ".identifier" do
-      it "creates an attribute and makes it the identifier" do
+    describe '.identifier' do
+      it 'creates an attribute and makes it the identifier' do
       end
     end
-    describe ".parent" do
-      it "creates a foreign_key attribute" do
+    describe '.parent' do
+      it 'creates a foreign_key attribute' do
       end
     end
-    describe ".link" do
-      it "creates a foreign_key attribute" do
+    describe '.link' do
+      it 'creates a foreign_key attribute' do
       end
     end
-    describe ".document" do
-      it "creates a document attribute" do
+    describe '.document' do
+      it 'creates a document attribute' do
       end
     end
-    describe ".image" do
-      it "creates an image attribute" do
+    describe '.image' do
+      it 'creates an image attribute' do
       end
     end
-    describe ".collection" do
-      it "creates a collection attribute" do
+    describe '.collection' do
+      it 'creates a collection attribute' do
       end
     end
   end
 
-  describe ".json_template" do
-    it "returns a json template describing the model" do
+  describe '.json_template' do
+    it 'returns a json template describing the model' do
     end
   end
 
-  describe ".schema" do
-    it "returns the sequel database schema for this model" do
+  describe '.schema' do
+    it 'returns the sequel database schema for this model' do
     end
   end
 
-  describe ".multi_update" do
-    it "updates multiple records at once from an array of json hashes" do
+  describe '.multi_update' do
+    it 'updates multiple records at once from an array of json hashes' do
     end
   end
 
-  describe ".update_or_create" do
-    it "updates or creates a record" do
+  describe '.update_or_create' do
+    it 'updates or creates a record' do
     end
   end
 
-  describe "#identifier" do
-    it "returns the value of the identifier for this record" do
+  describe '#identifier' do
+    it 'returns the value of the identifier for this record' do
     end
   end
 
-  describe "#run_loaders" do
-    it "runs the specified loader on a particular file" do
+  describe '#run_loaders' do
+    it 'runs the specified loader on a particular file' do
     end
   end
 
-  describe "#json_document" do
-    it "returns a json document describing this record" do
+  describe '#json_document' do
+    it 'returns a json document describing this record' do
     end
   end
 end
