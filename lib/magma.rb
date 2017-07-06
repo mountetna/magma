a16bc6792029054856136147477a0fd864999f41
Jason Cater <jason.cater@ucsf.edu>
2017-07-05 20:38:25 -0700

    Adding the project namespacing

--- lib/magma.rb
+++ lib/magma.rb
@@ -12,43 +12,56 @@ require 'singleton'
 
 class Magma
   include Singleton
+
+  # Database handle for the singleton.
   attr_reader :db
-  def connect db_config
-    @db = Sequel.connect( db_config )
+
+  def connect(db_config)
+    @db = Sequel.connect(db_config)
   end
 
-  def get_model name
+  def get_model(project_name, model_name)
+    nm = model_name.to_s.camel_case.to_sym
     begin
-      model = Kernel.const_get name.to_s.camel_case.to_sym
-      raise NameError unless model < Magma::Model
-      model
+      model = Kernel.const_get(project_name).const_get(nm)
+      raise(NameError) unless model < Magma::Model
+      return model
     rescue NameError => e
-      raise NameError, "Could not find Magma::Model #{name}"
+      err_txt = 'Could not find Magma::Model '
+      err_txt += namespaced_model(project_name, model_name)
+      raise(NameError, err_txt)
     end
+  end
+
+  # Appends the project name to the model name.
+  def namespaced_model(project_name, model_name)
+    :"#{project_name}::#{model_name.to_s.camel_case}"
   end
 
   def magma_models
     @magma_models ||= find_descendents Magma::Model
   end
 
-  def configure opts
+  def configure(opts)
     @config = opts
   end
 
-  def config type
+  def config(type)
     @config[environment][type]
   end
 
   def environment
-    (ENV["MAGMA_ENV"] || :development).to_sym
+    (ENV['MAGMA_ENV'] || :development).to_sym
   end
 
-  def load_models check_tables=true
+  def load_models(check_tables = true)
     connect(config :db)
+
     if config(:storage)
       require_relative 'magma/document'
       require_relative 'magma/image'
     end
+
     config(:project_path).split(/\s+/).each do |project_dir|
       base_file = File.join(File.dirname(__FILE__), '..', project_dir, 'requirements.rb')
       if File.exists?(base_file)
@@ -59,11 +72,13 @@ class Magma
         Dir.glob(File.join(File.dirname(__FILE__), '..', project_dir, 'metrics', '**', '*.rb'), &method(:require))
       end
     end
+
     if check_tables
       magma_models.each do |model|
         raise "Missing table for #{model}." unless model.has_table?
       end
     end
+
     carrier_wave_init
   end
 
@@ -72,7 +87,7 @@ class Magma
     db.pool.connection_validation_timeout = -1
   end
 
-  def find_descendents klass
+  def find_descendents(klass)
     ObjectSpace.each_object(Class).select do |k|
       k < klass
     end
@@ -88,7 +103,7 @@ class Magma
       config.fog_credentials = opts[:credentials]
       config.fog_directory = opts[:directory]
       config.fog_public = false
-      config.fog_attributes = { 'Cache-Control' => "max-age=#{365 * 86400}" }
+      config.fog_attributes = {'Cache-Control'=> "max-age=#{365 * 86400}"}
     end
   end
 end
--- lib/magma/attributes/link.rb
+++ lib/magma/attributes/link.rb
@@ -1,23 +1,35 @@
 class Magma
   module Link
+
+    # In the event (during model creation) that we use the variable
+    # '@link_model' to set the model 'type' for a link, we need to use that
+    # variable for for model retrieval. Otherwise the '@name' variable will
+    # contain the appropriate model type for a link.
     def link_model
-      Magma.instance.get_model(@link_model || @name)
+      if @link_model
+        return Magma.instance.get_model(@project, @link_model)
+      else
+        return Magma.instance.get_model(@project, @name)
+      end
     end
 
     def foreign_id
       :"#{@name}_id"
     end
 
+    # The model name has the project name appended to the front to form the
+    # namespace. An example is 'Ipi::Sample'. Here we need to strip off the 
+    # project name and append '_id'.
     def self_id
-      :"#{@model.name.snake_case}_id"
+      :"#{@model.name.snake_case.split('::')[1]}_id"
     end
 
-    def link_record identifier
-      link_model[ link_model.identity => identifier ]
+    def link_record(identifier)
+      link_model[link_model.identity=> identifier]
     end
 
-    def link_records identifiers
-      link_model.where( link_model.identity => identifiers )
+    def link_records(identifiers)
+      link_model.where(link_model.identity=> identifiers)
     end
 
     def link_identity
