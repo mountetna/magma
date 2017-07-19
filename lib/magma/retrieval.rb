4ebba89f26e85d6ed6cb70d897943e7a7bb7e188
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2017-07-19 01:46:26 +0000

    working filter test

--- lib/magma/retrieval.rb
+++ lib/magma/retrieval.rb
@@ -53,7 +53,61 @@ class Retrieval
     $/x
 
   def filter_term term
-    att_name, operator, value = term.match(FILTER_TERM)
+    match, att_name, operator, value = term.match(FILTER_TERM).to_a
+    raise ArgumentError, "Filter term '#{term}' does not parse" if match.nil?
+
+    att = @attributes.find{|a| a.name == att_name.to_sym}
+    raise ArgumentError, "#{att_name} is not an attribute" if att.nil?
+
+    case att
+    when Magma::CollectionAttribute, Magma::TableAttribute
+      raise ArgumentError, "Cannot filter on collection attributes"
+    when Magma::ForeignKeyAttribute, Magma::ChildAttribute
+      return [ att_name, '::identifier', string_op(operator), value ]
+    when Magma::Attribute
+      case att.type.name
+      when "Integer", "Float", "DateTime"
+        return [ att_name, numeric_op(operator), value ]
+      when "String"
+        return [ att_name, string_op(operator), value ]
+      when "TrueClass"
+        return [ att_name, boolean_op(operator, value) ]
+      else
+        raise ArgumentError, "Unknown type for attribute"
+      end
+    else
+      raise ArgumentError, "Cannot query for #{att_name}"
+    end
+  end
+
+  def string_op operator
+    case operator
+    when "="
+      return "::equals"
+    when "~"
+      return "::matches"
+    else
+      raise ArgumentError, "Invalid operator #{operator} for string attribute!"
+    end
+  end
+
+  def numeric_op operator
+    case operator
+    when "=", "<=", ">=", ">", "<"
+      return "::#{operator}"
+    else
+      raise ArgumentError, "Invalid operator #{operator} for string attribute!"
+    end
+  end
+
+  def boolean_op operator, value
+    raise ArgumentError, "Invalid operator #{operator} for boolean column!" unless operator == "="
+    case value
+    when "true"
+      return "::true"
+    when "false"
+      return "::false"
+    end
   end
 
   def outputs
