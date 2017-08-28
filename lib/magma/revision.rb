9467e5f7fe30843174dcccce42606f49d46522a1
Jason Cater <jason.cater@ucsf.edu>
2017-08-28 10:57:43 -0700

    Renaming validation to validator (verb to noun).

--- lib/magma/revision.rb
+++ lib/magma/revision.rb
@@ -1,37 +1,40 @@
 class Magma
   class Revision
     attr_reader :model, :record
+    attr_reader :errors
+
     def initialize(project_name, model_name, record_name, revised_document, validator)
       @model = Magma.instance.get_model(project_name, model_name)
       @record = @model[@model.identity => record_name]
       @revised_document = censored(revised_document || {})
       @validator = validator
     end
-    attr_reader :errors
 
     def valid?
       @errors = []
-      @validator.model_validation(@model).validate(@revised_document) do |error|
+
+      @validator.model_validator(@model).validate(@revised_document) do |error|
         @errors.push error
       end
 
       @errors.empty?
     end
 
+    # Update the record using this revision.
     def post!
-      # update the record using this revision
       @revised_document.each do |name, new_value|
         @model.attributes[name.to_sym].update @record, new_value
       end
       @record.save changed: true
-
       @record.refresh
     end
 
     private
 
-    def censored document
-      document.select do |att_name,val|
+    # Here we remove any attributes from the revised document that are set to
+    # 'read only'.
+    def censored(document)
+      document.select do |att_name, val|
         @model.has_attribute?(att_name) && !@model.attributes[att_name.to_sym].read_only?
       end
     end
