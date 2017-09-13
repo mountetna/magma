394da2e86739429e1cd2b2fd54ff00679e0ac343
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2017-09-13 22:17:08 +0000

    Add query and update specs for client.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -13,22 +13,41 @@ describe Magma::Client do
   end
 
   def client
-    @client ||= @configuration_class.instance
-  end
+    @client ||= 
+      begin
+        client = @configuration_class.instance
 
-  def client_post endpoint, content_type, body
-    post("/#{endpoint}", body, { 'CONTENT_TYPE'=> content_type })
-    last_response.define_singleton_method(:code) do
-      status
-    end
-    last_response
+        # Note that we are neither testing #post nor #multipart_post
+        allow(client).to receive(:multipart_post) do |endpoint, content|
+          path = "/#{endpoint}"
+          multipart = Net::HTTP::Post::Multipart.new(path, content)
+          body = multipart.body_stream.read
+          content_type = multipart.to_hash["content-type"].first
+
+          post(
+            "/#{endpoint}",
+            body,
+            'CONTENT_TYPE' => content_type
+          )
+          last_response.define_singleton_method(:code) do
+            status
+          end
+          last_response
+        end
+
+        allow(client).to receive(:post) do |endpoint, content_type, body|
+          post("/#{endpoint}", body, { 'CONTENT_TYPE'=> content_type })
+          last_response.define_singleton_method(:code) do
+            status
+          end
+          last_response
+        end
+
+        client
+      end
   end
 
   it "invokes the retrieve endpoint correctly." do
-    allow(client).to receive(:post) do |endpoint, content_type, body|
-      client_post(endpoint,content_type, body)
-    end
-
     token = 'janus-token'
 
     status = nil
@@ -50,6 +69,51 @@ describe Magma::Client do
 
     expect(status).to eq(200)
     expect(json[:models].keys).to eq([:labor])
+  end 
+
+  it "invokes the update endpoint correctly." do
+    token = 'janus-token'
+    lion = create(:monster, name: 'Nemean Lion', species: 'hydra')
+
+    status = nil
+    payload = nil
+    expect do
+      status, payload = client.update(
+        token,
+        'labors',
+        {
+          monster: {
+            'Nemean Lion' => {
+              species: 'lion'
+            }
+          }
+        }
+      )
+    end.to_not raise_error
+   
+    lion.refresh
+    expect(status).to eq(200)
+    expect(lion.species).to eq('lion')
+  end
+  
+  it "invokes the query endpoint correctly." do
+    token = 'janus-token'
+    create_list(:labor, 3)
+
+    status = nil
+    payload = nil
+    expect do
+      status, payload = client.query(
+        token,
+        'labors',
+        [ 'labor', '::all', '::identifier' ]
+      )
+    end.to_not raise_error
+   
+    json = JSON.parse(payload, symbolize_names: true)
+
+    expect(status).to eq(200)
+    expect(json[:answer].length).to eq(3)
   end
 end
 
