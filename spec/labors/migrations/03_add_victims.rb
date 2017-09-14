61589d2663094eb384931fb5895b122ac8a6f24f
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2017-09-14 18:43:26 +0000

    fix migration for victims

--- spec/labors/migrations/03_add_victims.rb
+++ spec/labors/migrations/03_add_victims.rb
@@ -1,10 +1,10 @@
 Sequel.migration do
   change do
-    create_table(:victims) do
+    create_table(:labors__victims) do
       primary_key :id
       DateTime :created_at
       DateTime :updated_at
-      foreign_key :monster_id, :monsters
+      foreign_key :monster_id, :labors__monsters
       index :monster_id
       String :name
       unique :name
