115a7fe06a38df93c9ef17a0c85c0f1483463bcb
Jason Cater <jason.cater@ucsf.edu>
2018-02-08 16:09:24 -0800

    Updating the migration code to handle namespaced (postgres schema) tables.

--- lib/magma/migration.rb
+++ lib/magma/migration.rb
@@ -2,7 +2,6 @@ class Magma
   class Migration
     class << self
       def create(model)
-        puts model.table_name
         if Magma.instance.db.table_exists?(model.table_name)
           return Magma::UpdateMigration.new(model)
         else
@@ -26,7 +25,7 @@ class Magma
     end
 
     def to_s
-      @changes.map do |key,lines|
+      @changes.map do |key, lines|
         str = SPC*2 + key + ' do' + "\n"
         lines.each do |line|
           str += SPC*3 + line + "\n"
@@ -40,34 +39,41 @@ class Magma
 
     SPC='  '
 
+    def namespaced_table_name(model_name)
+      project_name, table_name = model_name.split(/::/).map(&:snake_case)
+      table_name = table_name.plural
+      "#{project_name}__#{table_name}"
+    end
   end
+
   class CreateMigration < Migration
     def initialize(model)
       super
-      tlb_nm = "create_table(:#{model.table_name})"
+      tlb_nm = "create_table(:#{namespaced_table_name(model.name)})"
       change(tlb_nm, ['primary_key :id']+new_attributes)
     end
 
     def new_attributes
-      @model.attributes.map do |name,att|
+      @model.attributes.map do |name, att|
         next unless att.needs_column?
         att.migration(self)
       end.compact.flatten
     end
 
-    def foreign_key_entry column_name, foreign_table
-      "foreign_key :#{column_name}, :#{foreign_table}"
+    def foreign_key_entry(column_name, foreign_table)
+      table = "#{foreign_table.table.to_s}__#{foreign_table.column.to_s}".to_sym
+      "foreign_key :#{column_name}, :#{table}"
     end
 
-    def column_entry name, type
+    def column_entry(name, type)
       "#{type.name} :#{name}"
     end
 
-    def unique_entry name
+    def unique_entry(name)
       "unique :#{name}"
     end
 
-    def index_entry column
+    def index_entry(column)
       if column.is_a? Array
         "index [#{column.map{|c| ":#{c}"}.join(", ")}]"
       else
@@ -77,36 +83,53 @@ class Magma
   end
 
   class UpdateMigration < Migration
-    def initialize model
+    def initialize(model)
       super
-      change("alter_table(:#{model.table_name})", missing_attributes) unless missing_attributes.empty?
 
-      change("alter_table(:#{model.table_name})", removed_attributes) unless removed_attributes.empty?
+      unless missing_attributes.empty?
+        change(
+          "alter_table(:#{namespaced_table_name(model.name)})",
+          missing_attributes
+        )
+      end
 
-      change("alter_table(:#{model.table_name})", changed_attributes) unless changed_attributes.empty?
+      unless removed_attributes.empty?
+        change(
+          "alter_table(:#{namespaced_table_name(model.name)})",
+          removed_attributes
+        )
+      end
+
+      unless changed_attributes.empty?
+        change(
+          "alter_table(:#{namespaced_table_name(model.name)})",
+          changed_attributes
+        )
+      end
     end
 
-    def foreign_key_entry column_name, foreign_table
-      "add_foreign_key :#{column_name}, :#{foreign_table}"
+    def foreign_key_entry(column_name, foreign_table)
+      table = "#{foreign_table.table.to_s}__#{foreign_table.column.to_s}".to_sym
+      "add_foreign_key :#{column_name}, :#{table}"
     end
 
-    def column_type_entry name, type
+    def column_type_entry(name, type)
       "set_column_type :#{name}, #{type}"
     end
 
-    def column_entry name, type
-      "add_column :#{name}, #{type}"
+    def column_entry(name, type)
+      (type) ? "add_column :#{name}, #{type}" : "add_column :#{name}"
     end
 
-    def remove_column_entry name
+    def remove_column_entry(name)
       "drop_column :#{name}"
     end
 
-    def unique_entry name
+    def unique_entry(name)
       "add_unique_constraint :#{name}"
     end
 
-    def index_entry column
+    def index_entry(column)
       if column.is_a? Array
         "add_index [#{column.map{|c| ":#{c}"}.join(", ")}]"
       else
@@ -117,27 +140,26 @@ class Magma
     private 
 
     def missing_attributes
-      @model.attributes.map do |name,att|
+      @model.attributes.map do |name, att|
         next if att.schema_ok?
         att.migration(self)
       end.compact.flatten
     end
 
     def changed_attributes
-      @model.attributes.map do |name,att|
+      @model.attributes.map do |name, att|
         next unless att.schema_ok?
         next unless att.needs_column?
         next if att.schema_unchanged?
-        column_type_entry(att.column_name, 
-                          att.type)
+        column_type_entry(att.column_name, att.type)
       end.compact.flatten
     end
 
     def removed_attributes
       @model.schema.map do |name, db_opts|
         next if @model.attributes[name]
-        next if @model.attributes[ name.to_s.sub(/_id$/,'').to_sym ]
-        next if @model.attributes[ name.to_s.sub(/_type$/,'').to_sym ]
+        next if @model.attributes[name.to_s.sub(/_id$/,'').to_sym]
+        next if @model.attributes[name.to_s.sub(/_type$/,'').to_sym]
         next if db_opts[:primary_key]
         remove_column_entry(name)
       end.compact.flatten
