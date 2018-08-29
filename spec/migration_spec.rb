2e0ef943e9893467cb17975435c23e436f15bdfe
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2018-08-29 23:14:55 +0000

    add migration spec

--- spec/migration_spec.rb
+++ spec/migration_spec.rb
@@ -1,9 +1,16 @@
 describe Magma::Migration do
   before(:each) do
-    allow(Magma.instance).to receive(:config).and_return(:default)
-    allow(Magma.instance).to receive(:config).with(:project_path).and_return(:default)
+    #allow(Magma.instance).to receive(:config).and_return(:default)
+    #allow(Magma.instance).to receive(:config).with(:project_path).and_return(:default)
+
+    class Labors
+      class Olympian < Magma::Model
+        identifier :name
+      end
+    end
   end
 
   it 'suggests a migration' do
+    Olympian.migration
   end
 end
