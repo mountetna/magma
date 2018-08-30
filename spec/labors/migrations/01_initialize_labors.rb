9845c52892ef46f20c9e348d05124696241a5d55
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2018-08-30 16:26:13 +0000

    fix deprecated spec migrations

--- spec/labors/migrations/01_initialize_labors.rb
+++ spec/labors/migrations/01_initialize_labors.rb
@@ -1,38 +1,38 @@
 Sequel.migration do
   change do
-    create_table(:labors__projects) do
+    create_table(Sequel[:labors][:projects]) do
       primary_key :id
       DateTime :created_at
       DateTime :updated_at
       String :name
       unique :name
     end
-    create_table(:labors__labors) do
+    create_table(Sequel[:labors][:labors]) do
       primary_key :id
       DateTime :created_at
       DateTime :updated_at
-      foreign_key :project_id, :labors__projects
+      foreign_key :project_id, Sequel[:labors][:projects]
       index :project_id
       String :name
       unique :name
       Integer :number
       TrueClass :completed
     end
-    create_table(:labors__monsters) do
+    create_table(Sequel[:labors][:monsters]) do
       primary_key :id
       DateTime :created_at
       DateTime :updated_at
-      foreign_key :labor_id, :labors__labors
+      foreign_key :labor_id, Sequel[:labors][:labors]
       index :labor_id
       String :name
       unique :name
       String :species
     end
-    create_table(:labors__prizes) do
+    create_table(Sequel[:labors][:prizes]) do
       primary_key :id
       DateTime :created_at
       DateTime :updated_at
-      foreign_key :labor_id, :labors__labors
+      foreign_key :labor_id, Sequel[:labors][:labors]
       index :labor_id
       String :name
       unique :name
