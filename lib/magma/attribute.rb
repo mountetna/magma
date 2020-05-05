130dfa6488c9f5c153030f2efacff2fee6653e4c
Ali Ibrahim <aibrahim2k2@gmail.com>
2020-05-05 09:57:42 -0400

    Move Magma::Attribute::Validation to Magma::StringAttribute
      * Magma::Attribute::Validation was the default for all attributes, but it only
        applies to strings.

--- lib/magma/attribute.rb
+++ lib/magma/attribute.rb
@@ -164,25 +164,6 @@ class Magma
       end
     end
 
-    class Validation < Magma::Validation::Attribute::BaseAttributeValidation
-      def validate(value)
-        case match
-        when Regexp
-          yield format_error(value) if !match.match(value)
-        when Array
-          if !match.map(&:to_s).include?(value)
-            yield "On #{@attribute.name}, '#{value}' should be one of #{match.join(", ")}."
-          end
-        end
-      end
-
-      private
-
-      # memoize match to reuse across validations
-      def match
-        @match ||= @attribute.match.is_a?(Proc) ? @attribute.match.call : @attribute.match
-      end
-    end
     class Entry < Magma::BaseAttributeEntry
       def entry(value)
         [ @attribute.name, value ]
