3032f6765f464ee0f38eec51a465338716ce02b0
Cole S <4930129+coleshaw@users.noreply.github.com>
2020-05-12 18:32:42 +0000

    For File revisions, send signed request to Metis "copy" URL

--- lib/magma/server/update.rb
+++ lib/magma/server/update.rb
@@ -20,6 +20,11 @@ class UpdateController < Magma::Controller
 
     censor_revisions
 
+    # Unclear if this step to update file links in Metis should
+    # happen before load_revisions ... either could fail, which
+    # should cause the other to not run.
+    update_any_file_links if success?
+
     load_revisions if success?
 
     return success_json(@payload.to_hash) if success?
@@ -54,5 +59,81 @@ class UpdateController < Magma::Controller
   rescue Magma::LoadFailed => m
     log(m.complaints)
     @errors.concat(m.complaints)
+  end
+
+  def update_any_file_links
+    # Here, if there are any File attributes in the revisions,
+    #   we'll send a request to the Metis "copy" route, using the
+    #   Etna::Client.
+    # Assumes a conversion to Metis storage and no other
+    #   provider will be set.
+
+    # Etna::Client.new throws an exception if the host config includes protocol,
+    # i.e. https://metis-dev.etna-development.org will throw an exception
+    # raise Etna::BadRequest, 'Storage host should not include protocol' if
+    #   Magma.instance.config(:storage).fetch(:host).start_with? 'http'
+
+    auth = Etna::Auth.new(:magma)
+
+    host = Magma.instance.config(:storage).fetch(:host)
+
+    client = Etna::Client.new(
+      "https://#{host}",
+      @request.cookies[Magma.instance.config(:token_name)] || auth.send('auth', *[@request, :magma]))
+
+    copy_route = ''
+
+    client.routes.each do |route|
+      if route[:name] == 'copy'
+        copy_route = route
+        break
+      end
+    end
+
+    if copy_route == ''
+      return
+    end
+
+    @revisions.each do |model, model_revisions|
+      model_revisions.each do |revision|
+
+        # The below test for :stats as a File indicator seems
+        #   brittle -- anything better?
+        if revision.to_loader.key?(:stats)
+          path = client.send('route_path', *[copy_route, {:project_name => "foo", :bucket_name => "magma", :file_path => "test.txt"}])
+
+          hmac_signature = auth.send('etna_param', *[@request, :signature])
+
+          return false unless hmac_signature
+
+          return false unless headers = auth.send('etna_param', *[@request, :headers])
+
+          headers = headers.split(/,/).map do |header|
+            [ header.to_sym, auth.send('etna_param', *[@request, header]) ]
+          end.to_h
+
+          # Now expect the standard headers
+          hmac_params = {
+            method: 'post',
+            host: host,
+            path: path,
+
+            expiration: auth.send('etna_param', *[@request, :expiration]),
+            id: auth.send('etna_param', *[@request, :id]),
+            nonce: auth.send('etna_param', *[@request, :nonce]),
+            headers: headers,
+            test_signature: hmac_signature
+          }
+
+          hmac = Etna::Hmac.new(application, hmac_params)
+
+          # request.env['etna.hmac'] = hmac
+
+          return nil unless hmac.valid?
+
+          client.post(hmac.url_params[:path], hmac.url_params[:query])
+        end
+      end
+    end
   end
 end
