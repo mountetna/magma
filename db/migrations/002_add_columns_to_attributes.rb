5db7bf4d90db958f5ee1ebe747f1b1f1c22489f2
Ali Ibrahim <aibrahim2k2@gmail.com>
2020-05-15 15:26:44 -0400

    Make some attr columns not nullable
      * project_name, model_name, attribute_name, and type can't be null.
      * Also, set boolean defaults to false instead of "false" (Sequel will parse
        the string for boolean but not options like null).

--- db/migrations/002_add_columns_to_attributes.rb
+++ db/migrations/002_add_columns_to_attributes.rb
@@ -1,13 +1,17 @@
 Sequel.migration do
   change do
     alter_table(:attributes) do
-      add_column :type, String
-      add_column :hidden, "boolean", default: "false"
-      add_column :read_only, "boolean", default: "false"
-      add_column :unique, "boolean", default: "false"
-      add_column :index, "boolean", default: "false"
+      add_column :type, String, null: false
+      add_column :hidden, "boolean", default: false
+      add_column :read_only, "boolean", default: false
+      add_column :unique, "boolean", default: false
+      add_column :index, "boolean", default: false
       add_column :loader, String
-      add_column :restricted, "boolean", default: "false"
+      add_column :restricted, "boolean", default: false
+
+      set_column_not_null :project_name
+      set_column_not_null :model_name
+      set_column_not_null :attribute_name
     end
   end
 end
