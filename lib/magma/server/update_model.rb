42aa41cd51ac7dee511f23f5c9979eebf6a430a2
Mark Miranda <notmarkmiranda@gmail.com>
2020-06-08 09:36:07 -0600

    Add validate for update_model

--- lib/magma/server/update_model.rb
+++ lib/magma/server/update_model.rb
@@ -3,18 +3,24 @@ require_relative '../actions/update_attribute'
 
 class UpdateModelController < Magma::Controller
   def action
-    @params[:actions].each do |action_params|
+    actions = @params[:actions].map do |action_params|
       action_class = "Magma::#{action_params[:action_name].classify}".constantize
-      action_class.new(@project_name, action_params).perform
+      action_class.new(@project_name, action_params)
     end
 
-    @payload = Magma::Payload.new
 
-    Magma.instance.get_project(@project_name).models.each do |model_name, model|
-      retrieval = Magma::Retrieval.new(model, [], "all")
-      @payload.add_model(model, retrieval.attribute_names)
+    if actions.all? { |action| action.validate } 
+      actions.each(&:perform)
+      @payload = Magma::Payload.new
+
+      Magma.instance.get_project(@project_name).models.each do |model_name, model|
+        retrieval = Magma::Retrieval.new(model, [], "all")
+        @payload.add_model(model, retrieval.attribute_names)
+      end
+
+      return success(@payload.to_hash.to_json, 'application/json')
+    else
+      failure({}, 'application/json')
     end
-
-    return success(@payload.to_hash.to_json, 'application/json')
   end
 end
