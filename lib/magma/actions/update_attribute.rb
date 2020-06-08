60dbb6730be3e5d4b8ae1c94af45c7318b14e9ba
Mark Miranda <notmarkmiranda@gmail.com>
2020-06-08 09:36:07 -0600

    Add validate for update_model

--- lib/magma/actions/update_attribute.rb
+++ lib/magma/actions/update_attribute.rb
@@ -6,15 +6,26 @@ class Magma
     end
 
     def perform
-      model = Magma.instance.get_model(
+      @action_params.slice(*Magma::Attribute::EDITABLE_OPTIONS).each do |option, value|
+        attribute.update_option(option, value)
+      end
+    end
+
+    def model
+      @model ||= Magma.instance.get_model(
         @project_name,
         @action_params[:model_name]
       )
+    end
 
-      attribute = model.attributes[@action_params[:attribute_name].to_sym]
+    def attribute
+      @attribute ||= model.attributes[@action_params[:attribute_name].to_sym]
+    end
 
-      @action_params.slice(*Magma::Attribute::EDITABLE_OPTIONS).each do |option, value|
-        attribute.update_option(option, value)
+    def validate
+      return false unless attribute
+      @action_params.except(:action_name, :model_name, :attribute_name).keys.all? do |option|
+        attribute.respond_to?(option)
       end
     end
   end
--- spec/server/update_model_spec.rb
+++ spec/server/update_model_spec.rb
@@ -5,6 +5,9 @@ describe UpdateModelController do
     OUTER_APP
   end
 
+  let(:proper_name) { 'name' }
+  let(:improper_name) { 'namex' }
+
   it "rejects requests from non-superusers" do
     auth_header(:editor)
 
@@ -25,13 +28,13 @@ describe UpdateModelController do
     auth_header(:superuser)
     json_post(:update_model, {
       project_name: "labors",
-      actions: [
+      actions: [{
         action_name: "update_attribute",
         model_name: "monster",
-        attribute_name: "name",
+        attribute_name: proper_name,
         description: "The monster's name",
         display_name: "NAME"
-      ]
+      }]
     })
 
     expect(last_response.status).to eq(200)
@@ -42,5 +45,38 @@ describe UpdateModelController do
     attribute_json = response_json["models"]["monster"]["template"]["attributes"]["name"]
     expect(attribute_json["desc"]).to eq("The monster's name")
     expect(attribute_json["display_name"]).to eq("NAME")
+  end
+
+  it "does not update attribute options with invalid attribute name" do
+    auth_header(:superuser)
+    json_post(:update_model, {
+      project_name: "labors",
+      actions: [{
+        action_name: "update_attribute",
+        model_name: "monster",
+        attribute_name: improper_name,
+        description: "The monster's name",
+        display_name: "NAME"
+      }]
+    })
+    
+    expect(last_response.status).to eq(500)
+  end
+
+
+  it "does not update attribute options with the incorrect data type" do
+    auth_header(:superuser)
+    json_post(:update_model, {
+      project_name: "labors",
+      actions: [{
+        action_name: "update_attribute",
+        model_name: "monster",
+        attribute_name: 123,
+        description: "The monster's name",
+        display_name: "NAME"
+      }]
+    })
+
+    expect(last_response.status).to eq(500)
   end
 end
