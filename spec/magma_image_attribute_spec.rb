92bb76392eeb2e208a4e417b28502330fe955a56
Cole S <4930129+coleshaw@users.noreply.github.com>
2020-07-02 16:14:53 +0000

    additional tests for File and Image

--- spec/magma_image_attribute_spec.rb
+++ spec/magma_image_attribute_spec.rb
@@ -1,32 +1,34 @@
+# This should behave exactly like the File Attribute....
+# Except for thumbnails? May handle that on the Metis / Timur side.
 require_relative '../lib/magma'
 
-describe Magma::FileAttribute do
+describe Magma::ImageAttribute do
   it 'stores a JSON database type' do
-    file_attribute = Magma.instance.magma_projects[:labors].models[:monster].attributes[:stats]
+    image_attribute = Magma.instance.magma_projects[:labors].models[:monster].attributes[:selfie]
 
-    expect(file_attribute.database_type).to eq(:json)
+    expect(image_attribute.database_type).to eq(:json)
   end
 
   describe ".revision_to_loader" do
-    let(:file_attribute) { Magma.instance.magma_projects[:labors].models[:monster].attributes[:stats] }
+    let(:image_attribute) { Magma.instance.magma_projects[:labors].models[:monster].attributes[:selfie] }
     it 'returns entry when given valid path' do
-        expect(file_attribute.revision_to_loader("Nemean Lion", {
-            path: "metis://labors/Nemean Lion/lion-stats.txt"
+        expect(image_attribute.revision_to_loader("Nemean Lion", {
+            path: "metis://labors/Nemean Lion/lion-selfie.txt"
         })).to eq([
-            :stats,
+            :selfie,
             {
-                "filename": "monster-Nemean Lion-stats.txt",
+                "filename": "monster-Nemean Lion-selfie.txt",
                 "original_filename": nil,
-                "location": "metis://labors/Nemean Lion/lion-stats.txt"
+                "location": "metis://labors/Nemean Lion/lion-selfie.txt"
             }
         ])
     end
 
     it 'returns entry for ::blank path' do
-        expect(file_attribute.revision_to_loader("Nemean Lion", {
+        expect(image_attribute.revision_to_loader("Nemean Lion", {
             path: "::blank"
         })).to eq([
-            :stats,
+            :selfie,
             {
                 "filename": "::blank",
                 "original_filename": "::blank",
@@ -36,10 +38,10 @@ describe Magma::FileAttribute do
     end
 
     it 'returns nil entry for nil path' do
-        expect(file_attribute.revision_to_loader("Nemean Lion", {
+        expect(image_attribute.revision_to_loader("Nemean Lion", {
             path: nil
         })).to eq([
-            :stats,
+            :selfie,
             {
                 "filename": nil,
                 "original_filename": nil,
@@ -49,32 +51,32 @@ describe Magma::FileAttribute do
     end
 
     it 'returns nil for ::temp path' do
-        expect(file_attribute.revision_to_loader("Nemean Lion", {
+        expect(image_attribute.revision_to_loader("Nemean Lion", {
             path: "::temp"
         })).to eq(nil)
     end
   end
 
   describe ".revision_to_payload" do
-    let(:file_attribute) { Magma.instance.magma_projects[:labors].models[:monster].attributes[:stats] }
+    let(:image_attribute) { Magma.instance.magma_projects[:labors].models[:monster].attributes[:selfie] }
     let(:user) { Etna::User.new(email: "heracles@mountolympus.org", first: "Heracles", last: "of Thebes") }
     it 'returns full payload when given valid path' do
-        payload = file_attribute.revision_to_payload("Nemean Lion", {
-            path: "metis://labors/Nemean Lion/lion-stats.txt"
+        payload = image_attribute.revision_to_payload("Nemean Lion", {
+            path: "metis://labors/Nemean Lion/lion-selfie.txt"
         }, user)
         
-        expect(payload[0]).to eq(:stats)
-        expect(payload[1][:path]).to eq("monster-Nemean Lion-stats.txt")
+        expect(payload[0]).to eq(:selfie)
+        expect(payload[1][:path]).to eq("monster-Nemean Lion-selfie.txt")
         expect(payload[1][:original_filename]).to eq(nil)
         expect(payload[1][:url].is_a? URI).to eq(true)
         expect(payload[1][:url].query.include? "X-Etna-Signature=").to eq(true)
     end
 
     it 'returns simple path for ::blank path' do
-        expect(file_attribute.revision_to_payload("Nemean Lion", {
+        expect(image_attribute.revision_to_payload("Nemean Lion", {
             path: "::blank"
         }, user)).to eq([
-            :stats,
+            :selfie,
             {
                 path: "::blank"
             }
@@ -82,50 +84,50 @@ describe Magma::FileAttribute do
     end
 
     it 'returns nil entry for nil path' do
-        expect(file_attribute.revision_to_payload("Nemean Lion", {
+        expect(image_attribute.revision_to_payload("Nemean Lion", {
             path: nil
         }, user)).to eq([
-            :stats, nil
+            :selfie, nil
         ])
     end
 
     it 'returns signed upload Metis path for ::temp path' do
-        payload = file_attribute.revision_to_payload("Nemean Lion", {
+        payload = image_attribute.revision_to_payload("Nemean Lion", {
             path: "::temp"
         }, user)
         
-        expect(payload[0]).to eq(:stats)
+        expect(payload[0]).to eq(:selfie)
         expect(payload[1][:path].is_a? URI).to eq(true)
         expect(payload[1][:path].query.include? "X-Etna-Signature=").to eq(true)
     end
   end
 
   describe ".query_to_payload" do
-    let(:file_attribute) { Magma.instance.magma_projects[:labors].models[:monster].attributes[:stats] }
+    let(:image_attribute) { Magma.instance.magma_projects[:labors].models[:monster].attributes[:selfie] }
     
     it 'returns nil if no data passed in' do
-        expect(file_attribute.query_to_payload(nil)).to eq(nil)
+        expect(image_attribute.query_to_payload(nil)).to eq(nil)
     end
 
     it 'returns nil if no filename in the data' do
-        expect(file_attribute.query_to_payload({
+        expect(image_attribute.query_to_payload({
             key: 'value'
         })).to eq(nil)
     end
 
     it 'returns full query when given valid filename' do
-        query = file_attribute.query_to_payload({
-            filename: "monster-Nemean Lion-stats.txt"
+        query = image_attribute.query_to_payload({
+            filename: "monster-Nemean Lion-selfie.txt"
         })
         
-        expect(query[:path]).to eq("monster-Nemean Lion-stats.txt")
+        expect(query[:path]).to eq("monster-Nemean Lion-selfie.txt")
         expect(query[:original_filename]).to eq(nil)
         expect(query[:url].is_a? URI).to eq(true)
         expect(query[:url].query.include? "X-Etna-Signature=").to eq(true)
     end
 
     it 'returns simple query for ::blank filename' do
-        expect(file_attribute.query_to_payload({
+        expect(image_attribute.query_to_payload({
             filename: "::blank"
         })).to eq({
                 path: "::blank"
@@ -134,13 +136,13 @@ describe Magma::FileAttribute do
     end
 
     it 'returns nil for nil filename' do
-        expect(file_attribute.query_to_payload({
+        expect(image_attribute.query_to_payload({
             filename: nil
         })).to eq(nil)
     end
 
     it 'returns ::temp query for ::temp filename' do
-        expect(file_attribute.query_to_payload({
+        expect(image_attribute.query_to_payload({
             filename: "::temp"
         })).to eq({
             path: "::temp"
