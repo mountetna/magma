5a21d8cee9bb0ce9bcc929c7ebfc36329e1eecaf
Ali Ibrahim <aibrahim2k2@gmail.com>
2020-07-09 12:05:04 -0400

    Define Magma::AddModelAction validations

--- spec/actions/add_model_action_spec.rb
+++ spec/actions/add_model_action_spec.rb
@@ -83,4 +83,73 @@ describe Magma::AddModelAction do
       end
     end
   end
+
+  describe "#validate" do
+    context "when a required field is missing" do
+      let(:action_params) do
+        {
+          action_name: "add_attribute",
+          identifier: "name",
+          parent_model_name: "labor",
+          parent_link_type: "child"
+        }
+      end
+
+      it "returns false and adds an error" do
+        expect(action.validate).to eq(false)
+        expect(action.errors.first[:message]).to eq("model_name is required")
+      end
+    end
+
+    context "when a required field is not snake_case" do
+      let(:action_params) do
+        {
+          action_name: "add_attribute",
+          identifier: "name",
+          model_name: "newChildModel",
+          parent_model_name: "labor",
+          parent_link_type: "child"
+        }
+      end
+
+      it "returns false and adds an error" do
+        expect(action.validate).to eq(false)
+        expect(action.errors.first[:message]).to eq("model_name must be snake_case")
+      end
+    end
+
+    context "when the parent model doesn't exist" do
+      let(:action_params) do
+        {
+          action_name: "add_attribute",
+          model_name: "new_child_model",
+          identifier: "name",
+          parent_model_name: "houdini",
+          parent_link_type: "child"
+        }
+      end
+
+      it "returns false and adds an error" do
+        expect(action.validate).to eq(false)
+        expect(action.errors.first[:message]).to eq("parent_model_name does not match a model")
+      end
+    end
+
+    context "when parent_link_type is incorrect" do
+      let(:action_params) do
+        {
+          action_name: "add_attribute",
+          model_name: "new_child_model",
+          identifier: "name",
+          parent_model_name: "labor",
+          parent_link_type: "grandchild"
+        }
+      end
+
+      it "returns false and adds an error" do
+        expect(action.validate).to eq(false)
+        expect(action.errors.first[:message]).to eq("parent_link_type must be one of child, collection, table")
+      end
+    end
+  end
 end
