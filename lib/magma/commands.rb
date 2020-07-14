2ffe3694588f673286d87b872de6f95229f77871
Ali Ibrahim <aibrahim2k2@gmail.com>
2020-07-14 13:40:34 -0400

    Set Magma::Attribute#column_name in tests/code
      * Set column_name to attribute_name
      * When Magma::Attributes are created via the class interface, column_name will
        be set by default.
      * When Magma::Attributes are inserted directly into the database, column_name
        must be set manually.
      * Also use unpersisted Magma::Attributes in tests where possible.

--- lib/magma/commands.rb
+++ lib/magma/commands.rb
@@ -45,6 +45,7 @@ class Magma
         merge(
           project_name: project_name,
           model_name: model_name,
+          column_name: attribute[:attribute_name],
           type: attribute[:attribute_type],
           validation: Sequel.pg_json_wrap(attribute[:validation]),
         )
