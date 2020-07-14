5ee8c93234d6c28051ce0002b403c30ffcbe8938
Zachary Collins <recursive.cookie.jar@gmail.com>
2020-07-14 11:22:07 -0700

    Fixing magma migrations, templates, timur config, and ammending readme

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -3,23 +3,31 @@ set -e
 
 export PATH="/app/node_modules/.bin:/app/vendor/bundle/$RUBY_VERSION/bin:$PATH"
 
+function findEnvConfig() {
+  local splitLine=$(grep -n :test: config.yml | cut -d ':' -f 1)
+  if [[ $MAGMA_ENV == "test" ]]; then
+    tail -n+${splitLine} config.yml
+  else
+    head -n ${splitLine} config.yml
+  fi
+}
+
 if [ -z "$SKIP_RUBY_SETUP" ]; then
   bundle check || bundle install -j "$(nproc)"
-
   rm -f tmp/pids/*.pid
+
+  mkdir -p tmp/pids
+
   if [ -z "$SKIP_DB_WAIT" ]; then
     dockerize -wait tcp://magma_db:5432 -timeout 60s
 
-    for project in $(cat config.yml | grep ':project_path:' | sed -e 's/.*:project_path:\s*//g'); do
-      echo $project
+    for project in $(findEnvConfig | grep ':project_path:' | sed -e 's/.*:project_path:\s*//g'); do
+      echo "Initializing $project..."
       bin/magma create_db "$(basename $project)"
-      MAGMA_ENV=test bin/magma create_db "$(basename $project)"
-      bin/magma migrate "$(basename $project)"
-      MAGMA_ENV=test bin/magma migrate "$(basename $project)"
     done
 
-    bin/magma migrate
     bin/magma global_migrate
+    bin/magma migrate
   fi
 fi
 
