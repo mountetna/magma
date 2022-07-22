7f8fc4e865cadbbf467d754efc69798036772059
Cole <4930129+coleshaw@users.noreply.github.com>
2022-07-22 09:53:04 -0400

    fix insert syntax

--- db/migrations/010_add_link_attribute_name.rb
+++ db/migrations/010_add_link_attribute_name.rb
@@ -7,7 +7,7 @@ Sequel.migration do
     Magma.instance.db.execute("UPDATE attributes SET link_model_name=attribute_name WHERE attributes.link_model_name is NULL and attributes.type in ('parent', 'table', 'collection', 'child', 'link')")
 
     Magma.instance.db.execute("UPDATE attributes SET link_attribute_name=model_name WHERE attributes.link_model_name is not NULL and attribute_name!='reference_patient'")
-    Magma.instance.db.execute("INSERT into attributes SET (project_name, model_name, attribute_name, link_model_name, type, column_name, link_attribute_name) VALUES ('ipi', 'patient', 'panel_patients', 'patient', 'collection', 'panel_patients', 'reference_patient')")
+    Magma.instance.db.execute("INSERT into attributes (project_name, model_name, attribute_name, link_model_name, type, column_name, link_attribute_name) VALUES ('ipi', 'patient', 'panel_patients', 'patient', 'collection', 'panel_patients', 'reference_patient')")
     Magma.instance.db.execute("UPDATE attributes SET link_attribute_name='panel_patients' WHERE attribute_name='reference_patient' and project_name='ipi' and model_name='patient'")
   end
 
